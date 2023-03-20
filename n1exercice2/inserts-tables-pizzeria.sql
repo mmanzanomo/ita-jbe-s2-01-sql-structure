929200ffd9d5ee8a3763977ed2b2745f6645a4fb
Miguel Ángel Manzano <mznmiguel@gmail.com>
2023-03-20 18:12:18 +0100

    It was modified to restrict the categories to the product 'pizza'.

--- n1exercice2/inserts-tables-pizzeria.sql
+++ n1exercice2/inserts-tables-pizzeria.sql
@@ -16,32 +16,32 @@ INSERT INTO `pizzeria`.`provincias` (`id_provincia`,`nombre`) VALUES
 -------
 -- Localidades
 -------
-INSERT INTO `pizzeria`.`localidades` (`id_localidad`, `nombre`, `fk_provincias`) VALUES 
-    (1, 'Badalona', '1'),
-    (2, 'Barcelona', '1'),
-    (3, 'Figueras', '2'),
-    (4, 'Vendrell', '3'),
-    (5, 'Cambrils', '3'),
-    (6, 'Blanes', '2');
+INSERT INTO `pizzeria`.`localidades` (`id_localidad`, `nombre`, `id_provincia`) VALUES 
+    (1, 'Badalona', 1),
+    (2, 'Barcelona', 1),
+    (3, 'Figueras', 2),
+    (4, 'Vendrell', 3),
+    (5, 'Cambrils', 3),
+    (6, 'Blanes', 2);
 
 
 -------
 -- Direcciones
 -------
-INSERT INTO `pizzeria`.`direcciones` (`id_direccion`, `direccion`, `cp`, `fk_localidades`) VALUES 
-    (1, 'Plaza del Sol, 43', '08012', '2'),
-    (2, 'Carrer del Pintor Fortuny 7', '08001', '2'),
-    (3, 'Carrer de la Marina 129', '08018', '2'),
-    (4, 'Carrer del Mar, 53', '08918', '1'),
-    (5, 'Carrer de la Font, 25', '08911', '1'),
-    (6, 'Carrer de la Pau, 33', '43715', '4'),
-    (7, 'Carrer de la plaça, 6', '17300', '6');
+INSERT INTO `pizzeria`.`direcciones` (`id_direccion`, `direccion`, `cp`, `id_localidad`, `id_provincia`) VALUES 
+    (1, 'Plaza del Sol, 43', '08012', 2, 1),
+    (2, 'Carrer del Pintor Fortuny 7', '08001', 2, 1),
+    (3, 'Carrer de la Marina 129', '08018', 2, 1),
+    (4, 'Carrer del Mar, 53', '08918', 1, 1),
+    (5, 'Carrer de la Font, 25', '08911', 1, 1),
+    (6, 'Carrer de la Pau, 33', '43715', 4, 3),
+    (7, 'Carrer de la plaça, 6', '17300', 6, 2);
 
 
 -------
 -- Clientes
 -------
-INSERT INTO `pizzeria`.`clientes` (`id_cliente`, `nombre`, `apellidos`, `telefono`, `fk_direcciones`) VALUES 
+INSERT INTO `pizzeria`.`clientes` (`id_cliente`, `nombre`, `apellidos`, `telefono`, `id_direccion`) VALUES 
     (1, 'María', 'García', '666555444', 1),
     (2, 'Juan', 'Rodríguez', '677888999', 3),
     (3, 'Ana', 'López', '699123456', 2),
@@ -71,7 +71,7 @@ INSERT INTO `pizzeria`.`categorias` (`id_categoria`, `nombre`) VALUES
 -------
 -- Establecimientos
 -------
-INSERT INTO `pizzeria`.`establecimientos` (`id_establecimiento`, `fk_direcciones`) VALUES
+INSERT INTO `pizzeria`.`establecimientos` (`id_establecimiento`, `id_direccion`) VALUES
     (1, 1),
     (2, 2),
     (3, 3),
@@ -81,7 +81,7 @@ INSERT INTO `pizzeria`.`establecimientos` (`id_establecimiento`, `fk_direcciones
 -------
 -- Empleados
 -------
-INSERT INTO `pizzeria`.`empleados` (`id_empleado`, `nombre`, `apellidos`, `nif`, `telefono`, `es_repartidor`, `fk_establecimientos`) VALUES 
+INSERT INTO `pizzeria`.`empleados` (`id_empleado`, `nombre`, `apellidos`, `nif`, `telefono`, `es_repartidor`, `id_establecimientos`) VALUES 
     (1, 'Ana', 'García García', '12345678Z', '123456789', 0, 1),
     (2, 'David', 'Rodríguez Fernández', '98765432A', '987654321', 1, 2),
     (3, 'María', 'Pérez Sánchez', '23456789B', '654321987', 0, 3),
@@ -91,22 +91,22 @@ INSERT INTO `pizzeria`.`empleados` (`id_empleado`, `nombre`, `apellidos`, `nif`,
 -------
 -- Productos
 -------
-INSERT INTO `pizzeria`.`productos` (`id_producto`, `tipo`, `nombre`, `descripcion`, `imagen`, `precio`, `fk_categorias`, `fk_establecimientos`) VALUES 
-    (1, 'HAMBURGUESA', 'Hamburguesa Clásica', 'Sabrosa hamburguesa con carne de res, lechuga, tomate y cebolla', 'imagen_hamburguesa_clasica.png', 6.99, NULL, 1),
-    (2, 'BEBIDA', 'Refresco de cola', 'Refresco de cola carbonatado', 'imagen_refresco_cola.png', 2.99, NULL, 1),
-    (3, 'PIZZA', 'Pizza Cuatro Quesos', 'Exquisita pizza con mozzarella, parmesano, gorgonzola y queso de cabra', 'imagen_pizza_cuatro_quesos.png', 12.99, 1, 2),
-    (4, 'BEBIDA', 'Agua mineral', 'Agua mineral natural en botella de 500ml', 'imagen_agua_mineral.png', 1.99, NULL, 2),
-    (5, 'PIZZA', 'Pizza Hawaiana', 'Deliciosa pizza con jamón, piña y salsa de tomate', 'imagen_pizza_hawaiana.png', 10.99, NULL, 3),
-    (6, 'HAMBURGUESA', 'Hamburguesa Vegetariana', 'Sabrosa hamburguesa con falafel, lechuga, tomate y cebolla', 'imagen_hamburguesa_vegetariana.png', 8.99, NULL, 3),
-    (7, 'BEBIDA', 'Zumo de naranja', 'Zumo natural de naranja recién exprimido', 'imagen_zumo_naranja.png', 3.99, NULL, 3),
-    (8, 'PIZZA', 'Pizza Pepperoni', 'Exquisita pizza con pepperoni, queso mozzarella y salsa de tomate', 'imagen_pizza_pepperoni.png', 11.99, 7, 4),
-    (9, 'BEBIDA', 'Cerveza rubia', 'Cerveza rubia en lata de 330ml', 'imagen_cerveza_rubia.png', 4.99, NULL, 4);
+INSERT INTO `pizzeria`.`productos` (`id_producto`, `tipo`, `nombre`, `descripcion`, `imagen`, `precio`, `id_establecimientos`) VALUES 
+    (1, 'HAMBURGUESA', 'Hamburguesa Clásica', 'Sabrosa hamburguesa con carne de res, lechuga, tomate y cebolla', 'imagen_hamburguesa_clasica.png', 6.99, 1),
+    (2, 'BEBIDA', 'Refresco de cola', 'Refresco de cola carbonatado', 'imagen_refresco_cola.png', 2.99, 1),
+    (3, 'PIZZA', 'Pizza Cuatro Quesos', 'Exquisita pizza con mozzarella, parmesano, gorgonzola y queso de cabra', 'imagen_pizza_cuatro_quesos.png', 12.99, 2),
+    (4, 'BEBIDA', 'Agua mineral', 'Agua mineral natural en botella de 500ml', 'imagen_agua_mineral.png', 1.99, 2),
+    (5, 'PIZZA', 'Pizza Hawaiana', 'Deliciosa pizza con jamón, piña y salsa de tomate', 'imagen_pizza_hawaiana.png', 10.99, 3),
+    (6, 'HAMBURGUESA', 'Hamburguesa Vegetariana', 'Sabrosa hamburguesa con falafel, lechuga, tomate y cebolla', 'imagen_hamburguesa_vegetariana.png', 8.99, 3),
+    (7, 'BEBIDA', 'Zumo de naranja', 'Zumo natural de naranja recién exprimido', 'imagen_zumo_naranja.png', 3.99, 3),
+    (8, 'PIZZA', 'Pizza Pepperoni', 'Exquisita pizza con pepperoni, queso mozzarella y salsa de tomate', 'imagen_pizza_pepperoni.png', 11.99, 4),
+    (9, 'BEBIDA', 'Cerveza rubia', 'Cerveza rubia en lata de 330ml', 'imagen_cerveza_rubia.png', 4.99, 4);
 
 
 -------
 -- Pedidos
 -------
-INSERT INTO `pizzeria`.`pedidos` (`id_pedido`, `fecha_pedido`, `a_domicilio`, `precio_total`, `n_hamburguesas`, `n_bebidas`, `n_pizzas`, `hora_entrega`, `fk_clientes`, `fk_empleado_repartidor`) VALUES
+INSERT INTO `pizzeria`.`pedidos` (`id_pedido`, `fecha_pedido`, `a_domicilio`, `precio_total`, `n_hamburguesas`, `n_bebidas`, `n_pizzas`, `hora_entrega`, `id_cliente`, `id_empleado_repartidor`) VALUES
     (1, '2023-01-16 19:00:00', 0, 22.50, 1, 0, 2, NULL, 2, NULL),
     (2, '2023-02-17 12:15:00', 1, 45.25, 0, 2, 3, '2023-02-17 13:00:00', 3, 2),
     (3, '2023-02-18 20:45:00', 1, 30.75, 0, 1, 2, '2023-02-18 21:30:00', 4, 3),
@@ -119,7 +119,7 @@ INSERT INTO `pizzeria`.`pedidos` (`id_pedido`, `fecha_pedido`, `a_domicilio`, `p
 -------
 -- Pedidos tiene productos
 -------
-INSERT INTO `pizzeria`.`detalle_pedido` (`id_detalle_pedido`, `fk_productos`, `fk_pedidos`) VALUES
+INSERT INTO `pizzeria`.`detalle_pedido` (`id_detalle_pedido`, `id_productos`, `id_pedidos`) VALUES
     (1, 1, 1),
     (2, 3, 1),
     (3, 6, 1),
@@ -139,4 +139,14 @@ INSERT INTO `pizzeria`.`detalle_pedido` (`id_detalle_pedido`, `fk_productos`, `f
     (17, 2, 6),
     (18, 4, 7),
     (19, 3, 7);
+
+
+-------
+-- Pizza
+-------
+INSERT INTO `pizzeria`.`pizza` (`id_producto`, `id_categoria`) VALUES
+	(3, 1),
+	(5, 2),
+	(8, 3);
+    
 
--- n1exercice2/queries-pizzeria.sql
+++ n1exercice2/queries-pizzeria.sql
@@ -1,14 +1,14 @@
 -- Lista el número de bebidas vendidas en una determinada localidad.
 SELECT count(p.id_producto) as número_productos
 FROM productos p
-INNER JOIN establecimientos e ON e.id_establecimiento = p.fk_establecimientos
-INNER JOIN direcciones d ON d.id_direccion = e.fk_direcciones
-INNER JOIN localidades l ON l.id_localidad = d.fk_localidades
+INNER JOIN establecimientos e ON e.id_establecimiento = p.id_establecimientos
+INNER JOIN direcciones d ON d.id_direccion = e.id_direccion
+INNER JOIN localidades l ON l.id_localidad = d.id_localidad
 WHERE l.nombre = 'Barcelona' AND p.tipo = 'BEBIDA';
 
 
--- Lista cuantos pedidos ha realizado un determinado empleado.alter
+-- Lista cuantos pedidos ha realizado un determinado empleado.
 SELECT *
-FROM pedidos p INNER JOIN empleados e ON p.fk_empleado_repartidor = e.id_empleado
+FROM pedidos p INNER JOIN empleados e ON p.id_empleado_repartidor = e.id_empleado
 WHERE e.id_empleado = 2;
 
